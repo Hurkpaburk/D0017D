bea4076ccf7f78ba14d1cba14953671fa5a2c138
Hurk <johbe770@gmail.com>
2014-11-19 15:18:40 +0100

    Added bankomat menu

--- Bankomat/src/Bankomatjohbef.java
+++ Bankomat/src/Bankomatjohbef.java
@@ -19,28 +19,40 @@ public class Bankomatjohbef {
 	//------------------------------------------------------
 	public static void main(String []args){
 
-		int function = 1; // Init menu variable;
+		int[] trans = new int[10]; // declare and allocate array memory
+		// Change int[] number above to change size of transaction array 
+		
+		int balance;
+		int amount;
+		
+		Scanner keyboardInput = new Scanner(System.in); // Create object
 
+		int function = menu();		
+		
 		while (function != 4) {
-
-
-			function = menu();
 			
 			switch(function) {
-			case 1: // Withdraw
-
+			case 1: // Deposit
+				System.out.print("Amount to deposit: ");
+				amount = keyboardInput.nextInt();
+				makeTransaction(trans, amount);
+				
 			case 2: // Deposit
-
+				System.out.print("Amount to withdraw: ");
+				amount = -1*keyboardInput.nextInt();
+				makeTransaction(trans, amount);
 			case 3: // Balance
 
 			case 4: // Exit
 				break;
 
-			default: // Print Menu
-				function = menu();
+			default: // Do Nothing
+				System.out.println("Enter a number between 1 - 5!");
 			}
+			function = menu();
 		}
-
+		
+		keyboardInput.close();
 	}
 
 	//------------------------------------------------------
@@ -48,10 +60,11 @@ public class Bankomatjohbef {
 	// Inparametrar: None 
 	// Returvärde: userInput - User input 
 	//------------------------------------------------------
-	public static int menu()
-			throws IllegalArgumentException, InputMismatchException { 
+	public static int menu() { 
 
 		Scanner keyboardInput = new Scanner(System.in); // Create object
+		// complain about resource leak but it can not be closed in method <- gives exception errors for following inputs 
+		
 		int userInput;
 		
 		System.out.print("1. Insättning\n"
@@ -62,50 +75,48 @@ public class Bankomatjohbef {
 		
 		userInput = keyboardInput.nextInt();
 		
-		keyboardInput.close();
-		
 		return userInput;
-		
 	}
 
-//	//------------------------------------------------------
-//	// Beskrivning: Present the last transactions and balance
-//	// Inparametrar: trans - Transactions , balance - Account balance
-//	// Returvärde: None
-//	//------------------------------------------------------
-//	public static void showTransactions(int[] trans, int balance)
-//			throws IllegalArgumentException { 
-//		
-//
-//	}
-//
-//	//------------------------------------------------------
-//	// Beskrivning: Places amount in to transaction array
-//	// Inparametrar: trans - Transaction array, amount - Withdraw or deposit amount 
-//	// Returvärde: None
-//	//------------------------------------------------------
-//	public static void makeTransaction(int[] trans, int amount)
-//			throws IllegalArgumentException { 
-//
-//	}
-//
-//	//------------------------------------------------------
-//	// Beskrivning: Will return the position where transaction shall be placed
-//	// Inparametrar: trans - Transaction array
-//	// Returvärde: transPos - Transaction position in array 
-//	//------------------------------------------------------
-//	private static int findNr(int[] trans)
-//			throws IllegalArgumentException { 
-//
-//		return transPos;
-//	}
-//
-//	//------------------------------------------------------
-//	// Beskrivning: Moves all transaction in array one step to make room for new transaction 
-//	// Inparametrar: trans - Transaction array
-//	// Returvärde: None
-//	//------------------------------------------------------
-//	private static void moveTrans(int[] trans) {
-//	
-//	}
+	//------------------------------------------------------
+	// Beskrivning: Present the last transactions and balance
+	// Inparametrar: trans - Transactions , balance - Account balance
+	// Returvärde: None
+	//------------------------------------------------------
+	public static void showTransactions(int[] trans, int balance)
+			throws IllegalArgumentException { 
+		
+
+	}
+
+	//------------------------------------------------------
+	// Beskrivning: Places amount in to transaction array
+	// Inparametrar: trans - Transaction array, amount - Withdraw or deposit amount 
+	// Returvärde: None
+	//------------------------------------------------------
+	public static void makeTransaction(int[] trans, int amount)
+			throws IllegalArgumentException { 
+
+	}
+
+	//------------------------------------------------------
+	// Beskrivning: Will return the position where transaction shall be placed
+	// Inparametrar: trans - Transaction array
+	// Returvärde: transPos - Transaction position in array 
+	//------------------------------------------------------
+	private static int findNr(int[] trans) { 
+
+		int transPos = 0;
+		
+		return transPos;
+	}
+
+	//------------------------------------------------------
+	// Beskrivning: Moves all transaction in array one step to make room for new transaction 
+	// Inparametrar: trans - Transaction array
+	// Returvärde: None
+	//------------------------------------------------------
+	private static void moveTrans(int[] trans) {
+	
+	}
 }
